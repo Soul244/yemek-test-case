03821f21a481d506b3e537b454936676a2072cb8
Hasan Can Gedik <hasancangedik@hotmail.com>
2022-04-17 00:13:51 +0300

    removed bootstrap, reacstrap and example codes
    also added styled components

---
+++ pages/components/QuestionCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+
+function QuestionCard(props) {
+  return (
+    <div>QuestionCard</div>
+  )
+}
+
+QuestionCard.propTypes = {}
+
+export default QuestionCard
